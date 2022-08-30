5ed1bc5d74b55b195b1f16c19449695c91f4dd76
paramsd1997 <111410267+paramsd1997@users.noreply.github.com>
2022-08-30 17:41:32 +0530

    Update JhooqDockerDemoController.java

--- src/main/java/com/jhooq/demo/Jhooqdockerdemo/JhooqDockerDemoController.java
+++ src/main/java/com/jhooq/demo/Jhooqdockerdemo/JhooqDockerDemoController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class JhooqDockerDemoController {
 
-    @GetMapping("/hello")
+    @GetMapping("/jenkins")
     public String hello() {
         return "Deploying The Server Using Kubernetes using the Jenkins Contineous Integration Tool";
     }
